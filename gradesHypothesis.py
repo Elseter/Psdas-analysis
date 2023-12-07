34f37975c4d3f138f60e1953cf5cc2fe326ba5d6
Elseter <rking3425@gmail.com>
2023-12-07 15:49:54 -0500

    grades hypothesis

---
+++ gradesHypothesis.py
@@ -0,0 +1,10 @@
+"""
+* Developers: Riley / Emma
+* File name: scholarshipHypothesis.py
+* Class: COMSC.230
+* Prof: Dr. Omar
+* Purpose: This file runs a series of tests to analyze aspects of the
+*           Predict students' dropout and academic success dataset
+"""
+
+# HI EMMA
