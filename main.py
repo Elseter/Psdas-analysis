523300cc5a5a8d940ac8be95157c600e37db351d
Elseter <rking3425@gmail.com>
2023-12-07 10:44:11 -0500

    Initial Setup

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+* Developers: Riley / Emma
+* File name: main.py
+* Class: COMSC.230
+* Prof: Dr. Omar
+* Purpose: This file runs a series of tests to analyze aspects of the
+*           Predict students' dropout and academic success dataset
+"""
+
